3e9458d04d10d4d1f6b8e9e84acbb4aba655ca97
ilton <129966216+IltonFerpe@users.noreply.github.com>
2023-11-10 18:50:00 -0300

    alterei o readme

---
+++ MeLembreDasCompras/MeLembreDasComprasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLembreDasComprasApp.swift
+//  MeLembreDasCompras
+//
+//  Created by Ilton Ferpec on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MeLembreDasComprasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# MeLembreDasCompras
+Olá, me chamo Ilton Carlos e estou começando um novo projeto.
+Um aplicativo nativo iOS que ajuda a lembrar do que comprar e também dizer onde a compra com os itens adiconados ficará mais barato de acordo com a região.
+
+Espero que gostem!
